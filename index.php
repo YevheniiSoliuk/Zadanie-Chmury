bd15cb7c41a7aed07b788748267c74c0717cf24a
Yevhenii Soliuk <y.soliuk02@gmail.com>
2022-05-03 21:20:15 +0200

    new file:   Dockerfile 	modified:   index.php 	modified:   info.log 	new file:   info.txt

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<?php
+    //wyłączenie uprzedzeń w PHP
+    //error_reporting(E_ALL & ~E_NOTICE);
+    //Zapisanie informacji o dacie uruchomienia, imieniu i nazwisku autora oraz porcie TCP do pliku info.log
+    file_put_contents("info.txt", "Data uruchomienia ".date("d-m-Y")." "."Autor skryptu: Yevhenii Soliuk"." "."port TCP:" . $_SERVER['SERVER_PORT'] . PHP_EOL, FILE_APPEND);
+
+    //pobranie adresu IP klienta
+    $ip = $_SERVER['REMOTE_ADDR'];
+    //pobranie API z informacją o miejscu lokalizacji użytkownika
+    $ipInfo = file_get_contents('http://ip-api.com/json/' . $ip);
+    //dekodowanie pliku JSON do obiektu
+    $ipInfo = json_decode($ipInfo); 
+    //pobranie strefy czasowej z obiektu
+    $timezone = $ipInfo->timezone;
+    //ustawienie standardowej strefy czasowej, jako strefy czasowej użytkownika
+    date_default_timezone_set($timezone);
+
+    //Wyświetlenie po stronie klienta jego adresu IP, strefy czasowej oraz daty i czasu
+    // echo "<h1 style='text-align: center; text-decoration: underline;'> Your IP address: ". $ip ." </h1>";
+    // echo "<strong>Twoja strefa czasowa:</strong> " . date_default_timezone_get()."<br>";
+    // echo "<strong>Data i czas:</strong> " . date('Y/m/d H:i:s');
+  ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,20 +29,12 @@
   <title>Cloud serwer</title>
 </head>
 <body style="background-color: lightblue;">
-  <?php
-  file_put_contents("info.log", "Data uruchomienia ".date("d-m-Y")." "."Autor skryptu: Yevhenii Soliuk"." "."port TCP:" . $_SERVER['SERVER_PORT'] . PHP_EOL, FILE_APPEND);
-
-  echo "<h1 style='text-align: center; text-decoration: underline;'> Your IP address: ". $_SERVER['REMOTE_ADDR'] ." </h1>";
-
-  $ip = $_SERVER['REMOTE_ADDR']; //"189.240.194.147"
-  $ipInfo = file_get_contents('http://ip-api.com/json/' . $ip);
-  $ipInfo = json_decode($ipInfo);
-  $timezone = $ipInfo->timezone;
-  date_default_timezone_set($timezone);
-  echo "Twoja strefa czasowa: " . date_default_timezone_get()."<br>";
-  echo "Data i czas: " . date('Y/m/d H:i:s');
-  ?>
-
+  <h1>Hello world!</h1>
+  
+  <h1 style='text-align: center; text-decoration: underline;'> Your IP address:<?php $ip ?></h1>
+  <strong>Twoja strefa czasowa:</strong> <?php date_default_timezone_get() ?> <br>
+  <p>
+    <strong>Data i czas:</strong><?php date('Y/m/d H:i:s') ?>
+  </p>
 </body>
 </html>
-
